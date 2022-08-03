cbb2e2cd8fc6ad12acf426c13ddc08637d73d3bf
JWittmeyer <jens.wittmeyer@onetask.ai>
2022-08-03 14:08:09 +0200

    Changes the windows ipconfig extraction

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ set LOCAL_VOLUME_MINIO=".\minio-data"
 set LOCAL_VOLUME_QDRANT=".\qdrant-data"
 
 rem grab MINIO_ENDPOINT from ipconfig
-set ip_address_string="IPv4 Address"
+set ip_address_string="IPv4"
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
 	set ip=%%f 
 )
