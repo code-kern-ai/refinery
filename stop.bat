78d8d4aa0fbe53e3e26fc9ca7a4692877b2bac31
Felix Kirsch <40427105+FelixKirsch@users.noreply.github.com>
2022-09-27 19:19:33 +0200

    adds error messages (#109)

--- stop.bat
+++ stop.bat
@@ -1,4 +1,9 @@
 @echo off
 
+echo "We are currently experiencing some issues with refinery."
+echo "We are working on it. Sorry, for the inconvenience."
+echo "If you pull the repository and this message is still there, please try to pull it at a later time."
+exit 1
+
 docker-compose -f refinery\docker-compose.yml down --remove-orphans
 if "%1" neq "update" pause
