349a0bfc331e24dce357463a52074eebfae1f39d
felix0496 <felix.kirsch@kern.ai>
2022-10-10 16:42:01 +0200

    fixes version number for alfred

--- start.bat
+++ start.bat
@@ -1,10 +1,8 @@
 @echo off
 
-:: docker pull kernai/alfred:latest
-
 docker run -d --rm --name alfred^
 -v /var/run/docker.sock:/var/run/docker.sock^
--v %cd%\refinery:/refinery kernai/alfred:latest^
+-v %cd%\refinery:/refinery kernai/alfred:v1.3.4^
 python start.py %cd%\refinery > nul
 
 docker logs -f alfred
--- update.bat
+++ update.bat
@@ -1,4 +1,8 @@
 @echo off
 
+call start.bat
+
 git checkout main
 git pull
+
+call stop.bat
