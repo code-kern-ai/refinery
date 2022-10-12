b60fc103ec7ea61536b21e42e610ed34d6be4454
felix0496 <40427105+felix0496@users.noreply.github.com>
2022-10-12 15:50:31 +0200

    changes current directory in start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,19 @@
 @echo off
 
-docker pull kernai/alfred:v1.3.5
+:: path of the working directory with slashes instead of backslashes
+:: removes the colon, and prepends a slash
+set "PWD=%cd%"
+set "PWD=%PWD:\=/%"
+set "PWD=%PWD::=%"
+set "PWD=/%PWD%"
+
+docker pull kernai/alfred:v1.3.4
 
 docker run -d --rm ^
 --name alfred ^
 -v /var/run/docker.sock:/var/run/docker.sock ^
--v %cd%\refinery:/refinery kernai/alfred:v1.3.5 ^
-python start.py %cd%\refinery windows > nul
+-v %PWD%/refinery:/refinery kernai/alfred:v1.3.4 ^
+python start.py %PWD%/refinery windows > nul
 
 docker logs -f alfred
 
