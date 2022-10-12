3befc966a1f3515f7390255fe142176b40d31b24
felix0496 <felix.kirsch@kern.ai>
2022-10-12 12:23:42 +0200

    changes start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,12 @@
 @echo off
 
-docker run -d --rm --name alfred^
--v /var/run/docker.sock:/var/run/docker.sock^
--v %cd%\refinery:/refinery kernai/alfred:v1.3.4^
-python start.py %cd%\refinery > nul
+docker pull kernai/alfred:v1.3.5
+
+docker run -d --rm ^
+--name alfred ^
+-v /var/run/docker.sock:/var/run/docker.sock ^
+-v %cd%\refinery:/refinery kernai/alfred:v1.3.5 ^
+python start.py %cd%\refinery windows > nul
 
 docker logs -f alfred
 
